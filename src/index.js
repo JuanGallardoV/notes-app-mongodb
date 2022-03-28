8a56ce82d47a4c912e3b238738df94723ba95b6a
Juan Gallardo <juangallardovergara17@gmail.com>
2022-03-28 19:29:06 -0300

    Configuradas las configs necesarias

---
+++ src/index.js
@@ -0,0 +1,38 @@
+//? Juan Gallardo 28-03-2022
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
+//? Inicializaciones
+const app = express()
+
+//? Configuraciones
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+//? Middlewares
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.use(session({
+    secret: 'mysecretapp',
+    resave: true,
+    saveUninitialized: true
+}))
+
+//? Variables globales
+
+//? Rutas
+
+//? Archivos estaticos
+
+//? El servidor esta escuchando
+app.listen(app.get('port'), () => {
+    console.log('Server en puerto', app.get('port'))
+})
