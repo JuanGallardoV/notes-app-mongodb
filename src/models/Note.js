76dcd49b3c6cf631af9ac4a7131e860b4f8e4673
Juan Gallardo <juangallardovergara17@gmail.com>
2022-03-29 16:37:58 -0300

    Creado el CRUD de notas

---
+++ src/models/Note.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require('mongoose')
+const { Schema } = require('mongoose')
+
+const NoteSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('Note', NoteSchema)
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,7 +1,56 @@
 const router = require('express').Router()
 
-router.get('/notes', (req, res) => {
-    res.send('Notas desde la base de datos')
+const Note = require('../models/Note')
+
+router.get('/notes/add', (req,res) => {
+    res.render('notes/addNote')
+})
+
+router.post('/notes/newNote', async (req,res) => {
+    const { title, description } = req.body
+    const errors = []
+    if(!title) {
+        errors.push({text: 'El titulo no puede estar vacio'})
+    }
+    if(!description) {
+        errors.push({text: 'La descripcion no puede estar vacia'})
+    }
+    if(errors.length > 0) {
+        res.render('notes/addNote', {
+            errors,
+            title,
+            description
+        })
+    } else {
+        const newNote = new Note({title, description})
+        await newNote.save()
+        req.flash('success_msg', 'Nota Agregada Correctamente')
+        res.redirect('/notes')
+    }
+})
+
+router.get('/notes', async (req, res) => {
+    //! Sin el lean falla, ya que lo que hace el lean es transformar el documento de Mongo a un documento plano, y asi poder leerlo 
+    const notes = await Note.find().lean().sort({date: 'desc'})
+    res.render('notes/listNotes', { notes })
+})
+
+router.get('/notes/edit/:id', async (req, res) => {
+    const note = await Note.findById(req.params.id).lean()
+    res.render('notes/editNote', { note })
+})
+
+router.put('/notes/editNote/:id', async (req, res) => {
+    const { title, description } = req.body
+    await Note.findByIdAndUpdate(req.params.id, { title, description })
+    req.flash('success_msg', 'Nota Actualizada Correctamente')
+    res.redirect('/notes')
+})
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id)
+    req.flash('success_msg', 'Nota Eliminada Correctamente')
+    res.redirect('/notes')
 })
 
 module.exports = router
