4bb27c31bcff5b7f100ffbcf0f6420b63d7a4d81
Juan Gallardo <juangallardovergara17@gmail.com>
2022-03-29 19:17:22 -0300

    Implementado el login de usuarios

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
 //? Inicializaciones
 const app = express()
 require('./database')
+require('./config/passport')
 //? Configuraciones
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
@@ -27,11 +29,14 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 //? Variables globales
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
+    res.locals.error = req.flash('error')
     next()
 })
 //? Rutas
