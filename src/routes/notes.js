c7a24fcb59eb803d3e457a4ab36741c27d59fcbc
Juan Gallardo <juangallardovergara17@gmail.com>
2022-03-29 20:36:13 -0300

    Agregada la navegacion entre paginas

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 
 const Note = require('../models/Note')
+const { isAuthenticated } = require('../helpers/auth')
 
-router.get('/notes/add', (req,res) => {
+router.get('/notes/add', isAuthenticated, (req,res) => {
     res.render('notes/addNote')
 })
 
-router.post('/notes/newNote', async (req,res) => {
+router.post('/notes/newNote', isAuthenticated, async (req,res) => {
     const { title, description } = req.body
     const errors = []
     if(!title) {
@@ -29,25 +30,25 @@ router.post('/notes/newNote', async (req,res) => {
     }
 })
 
-router.get('/notes', async (req, res) => {
+router.get('/notes', isAuthenticated, async (req, res) => {
     //! Sin el lean falla, ya que lo que hace el lean es transformar el documento de Mongo a un documento plano, y asi poder leerlo 
     const notes = await Note.find().lean().sort({date: 'desc'})
     res.render('notes/listNotes', { notes })
 })
 
-router.get('/notes/edit/:id', async (req, res) => {
+router.get('/notes/edit/:id', isAuthenticated, async (req, res) => {
     const note = await Note.findById(req.params.id).lean()
     res.render('notes/editNote', { note })
 })
 
-router.put('/notes/editNote/:id', async (req, res) => {
+router.put('/notes/editNote/:id', isAuthenticated, async (req, res) => {
     const { title, description } = req.body
     await Note.findByIdAndUpdate(req.params.id, { title, description })
     req.flash('success_msg', 'Nota Actualizada Correctamente')
     res.redirect('/notes')
 })
 
-router.delete('/notes/delete/:id', async (req, res) => {
+router.delete('/notes/delete/:id', isAuthenticated, async (req, res) => {
     await Note.findByIdAndDelete(req.params.id)
     req.flash('success_msg', 'Nota Eliminada Correctamente')
     res.redirect('/notes')
